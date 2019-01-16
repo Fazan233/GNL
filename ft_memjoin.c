6c85a52b0253747582c93ae88696a05346e220f7
Vladyslav USLYSTYI <vuslysty@e2r11p11.unit.ua>
2019-01-16 18:01:11 +0200

    GOOD GNL

--- ft_memjoin.c
+++ ft_memjoin.c
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   ft_memjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vuslysty <vuslysty@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/16 17:42:07 by vuslysty          #+#    #+#             */
-/*   Updated: 2019/01/16 17:43:14 by vuslysty         ###   ########.fr       */
+/*   Created: 2019/01/16 17:44:12 by vuslysty          #+#    #+#             */
+/*   Updated: 2019/01/16 17:44:23 by vuslysty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_GET_NEXT_LINE_H
-# define PRINTF_GET_NEXT_LINE_H
-# define BUFF_SIZE 32
-# include <unistd.h>
+#include "libft.h"
 
-int					get_next_line(const int fd, char **line);
-
-typedef struct		s_fdlst
+void	*ft_memjoin(void *mem1, size_t len1, void *mem2, size_t len2)
 {
-	char			*data;
-	int				fd;
-	struct s_fdlst	*next;
-}					t_fdlst;
+	void	*buf;
 
-#endif
+	buf = malloc(len1 + len2 + 1);
+	ft_memmove(buf, mem1, len1);
+	ft_memmove(buf + len1, mem2, len2);
+	return (buf);
+}
--- get_next_line.c
+++ get_next_line.c
@@ -5,127 +5,113 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vuslysty <vuslysty@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/08 13:42:11 by vuslysty          #+#    #+#             */
-/*   Updated: 2018/11/15 15:13:25 by vuslysty         ###   ########.fr       */
+/*   Created: 2019/01/16 16:16:03 by vuslysty          #+#    #+#             */
+/*   Updated: 2019/01/16 17:41:24 by vuslysty         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "libft/libft.h"
 
-static t_list		*ft_listnew(int fd)
+static void		delete_unused_fd(t_fdlst **begin, t_fdlst *lst)
 {
-	t_list			*list;
+	t_fdlst			*tmp;
 
-	list = (t_list*)ft_memalloc(sizeof(t_list));
-	if (list)
+	tmp = *begin;
+	if (*begin == lst)
 	{
-		list->next = NULL;
-		list->content = (char*)ft_memalloc(BUFF_SIZE + 1);
-		if (list->content)
-			list->content_size = fd;
-		else
-		{
-			free(list);
-			return (NULL);
-		}
+		tmp = tmp->next;
+		free((*begin)->data);
+		free(*begin);
+		*begin = tmp;
 	}
-	return (list);
+	else
+	{
+		while (tmp->next != lst)
+			tmp = tmp->next;
+		tmp->next = tmp->next->next;
+		free(lst->data);
+		free(lst);
+	}
 }
 
-static int			is_endline(t_list *list, char **all_str)
+static int		check_endline(t_fdlst *list, char **line)
 {
 	int				i;
-	char			*temp;
-	char			buf[BUFF_SIZE + 1];
-	char			*str_buf;
+	int				len;
+	char			*tmp;
 
-	str_buf = (char*)list->content;
 	i = -1;
-	temp = *all_str;
-	while (str_buf[++i] != '\0')
-		if (str_buf[i] == '\n')
+	len = ft_strlen(*line);
+	while (list->data[++i] != '\0')
+	{
+		if (list->data[i] == '\n')
 		{
-			ft_strncpy(buf, str_buf, i);
-			buf[i] = '\0';
-			*all_str = ft_strjoin(temp, buf);
-			free(temp);
-			ft_strncpy(str_buf, str_buf + i + 1, BUFF_SIZE);
+			tmp = *line;
+			*line = ft_memjoin(*line, len, list->data, i);
+			free(tmp);
+			(*line)[len + i] = '\0';
+			ft_strcpy(list->data, list->data + i + 1);
 			return (1);
 		}
-	*all_str = ft_strjoin(temp, str_buf);
-	free(temp);
-	ft_bzero(str_buf, BUFF_SIZE);
+	}
+	tmp = *line;
+	*line = ft_memjoin(*line, len, list->data, i);
+	(*line)[i + len] = '\0';
+	free(tmp);
 	return (0);
 }
 
-static t_list		*get_right_list(int fd, t_list *begin)
+static t_fdlst	*get_right_list(const int fd, t_fdlst **list)
 {
-	t_list			*temp;
+	t_fdlst			*tmp;
 
-	temp = begin;
-	if (!begin->content)
-		begin->content = (char*)ft_memalloc(BUFF_SIZE + 1);
-	while (temp->next != NULL)
+	tmp = *list;
+	if (tmp == NULL)
 	{
-		if ((int)temp->content_size == fd)
-			return (temp);
-		temp = temp->next;
+		*list = (t_fdlst*)malloc(sizeof(t_fdlst));
+		(*list)->fd = fd;
+		(*list)->data = ft_memalloc(BUFF_SIZE + 1);
+		(*list)->next = NULL;
+		tmp = *list;
 	}
-	if ((int)temp->content_size == fd)
-		return (temp);
 	else
-		ft_lstadd_end(&begin, ft_listnew(fd));
-	return ((temp->next) ? temp->next : NULL);
-}
-
-/*
-** The function *validator* return 3 values: -1, 0, 1
-** -1 - when discriptor is not valid;
-** 0  - when file is valid and you can read it;
-** 1  - when file descriptor moved to end
-*/
-
-static int			validator(t_list *list)
-{
-	int				rd;
-	int				fd;
-	char			buf[1];
-
-	fd = (int)list->content_size;
-	if (fd >= 0)
 	{
-		rd = read(fd, buf, 1);
-		if (rd == 1)
-			((char*)list->content)[ft_strlen(((char*)list->content))] = *buf;
-		if (rd == 1 || rd == 0)
-			return ((rd == 0) ? 1 : 0);
-	}
-	return (-1);
-}
-
-int					get_next_line(const int fd, char **line)
-{
-	static t_list	list = {NULL, 0, NULL};
-	t_list			*temp;
-	char			*all_str;
-	int				rd;
-	int				valid;
-
-	all_str = ft_strdup("");
-	temp = get_right_list(fd, &list);
-	VALID_LIST(temp);
-	if (((char*)(temp->content))[0] == '\0')
-		if ((valid = validator(temp)))
-			return ((valid == 1) ? 0 : -1);
-	if (is_endline(temp, &all_str))
-		rd = 1;
-	else
-		while ((rd = read(fd, (char*)temp->content, BUFF_SIZE)))
+		while (tmp != NULL && tmp->fd != fd)
+			tmp = tmp->next;
+		if (tmp == NULL)
 		{
-			((char*)temp->content)[rd] = '\0';
-			if (is_endline(temp, &all_str))
-				break ;
+			tmp = (t_fdlst*)malloc(sizeof(t_fdlst));
+			tmp->fd = fd;
+			tmp->data = ft_memalloc(BUFF_SIZE + 1);
+			tmp->next = *list;
+			*list = tmp;
 		}
-	*line = all_str;
-	GNL_RETURN(rd);
+	}
+	return (tmp);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static t_fdlst	*list;
+	t_fdlst			*tmp;
+	int				rd;
+
+	if (fd < 0 || !line)
+		return (-1);
+	*line = ft_strdup("");
+	tmp = get_right_list(fd, &list);
+	if (check_endline(tmp, line))
+		return (1);
+	while ((rd = read(tmp->fd, tmp->data, BUFF_SIZE)) && rd != -1)
+	{
+		tmp->data[rd] = '\0';
+		if (check_endline(tmp, line))
+			return (1);
+	}
+	delete_unused_fd(&list, tmp);
+	if (**line != 0)
+		return (1);
+	free(*line);
+	return (rd == -1 ? -1 : 0);
 }
